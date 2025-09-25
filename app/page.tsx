5342e4f84d7f2afa2156e00c8725660bae03f2dc
jhonlaylopez <jhonlaylopez.2003@gmail.com>
2025-09-25 09:51:36 +0800

    implement follow feature

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
-import CreatePost from "@/components/CreatePost";
-
 import { currentUser } from "@clerk/nextjs/server";
+
+import CreatePost from "@/components/CreatePost";
+import WhoToFollow from "@/components/WhoToFollow";
 
 export default async function Home() {
   const user = await currentUser();
@@ -8,7 +9,7 @@ export default async function Home() {
     <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
       <div className="lg:col-span-6">{user ? <CreatePost /> : null}</div>
       <div className="hidden lg:block lg:col-span-4 sticky top-20">
-        Who to Follow
+        <WhoToFollow />
       </div>
     </div>
   );
